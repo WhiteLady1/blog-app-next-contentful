276e0f459636bed62be639b40056821226524f8c
Blanka Semanova <bsemanova@deloittece.com>
2023-11-20 10:30:30 +0100

    Remove console.log from homepage and edited semicolonst

--- app/page.tsx
+++ app/page.tsx
@@ -32,40 +32,17 @@ const getHomepageData = async () => {
   return res.items[0];
 };
 
-const getBlogPosts = async () => {
-  const res = await client.getEntries<BlogPostSkeleton>({ content_type: 'blogPost' });
-  return res.items;
-};
-
 export default async function Home() {
   const homepageData = await getHomepageData();
-  const blogPosts = await getBlogPosts();
 
-  // const selectedPostsTest = homepageData.fields.selectedPosts as Entry<BlogPostSkeleton , undefined, string>[];
+  const selectedPosts = homepageData.fields.selectedPost as Entry<BlogPostSkeleton , undefined, string>[];
   const contact = homepageData.fields.contact as Entry<ContactSkeleton, undefined, string>;
-
-  console.log(homepageData.fields.selectedPost);
 
   return (
     <div className='homepage'>
       <h1 className='homepage__title'>{homepageData.fields.title}</h1>
       <div className='homepage__selected-posts'>
-        {blogPosts.map(post => {
-          const image = post.fields.image as Asset<undefined, string>
-          return (
-            <BlogPostPreview
-              key={post.sys.id}
-              title={post.fields.title}
-              href={`/posts/${post.fields.slug}`}
-              imageUrl={`https:${image.fields.file?.url}`}
-              imageWidth={image.fields.file?.details.image?.width || 200}
-              imageHeight={image.fields.file?.details.image?.height || 100}
-            />
-          )
-        })}
-      </div>
-      {/* <div>
-        {selectedPostsTest && selectedPostsTest.map(post => {
+        {selectedPosts.map(post => {
           const image = post.fields.image as Asset<undefined, string>;
           return (
             <BlogPostPreview
@@ -78,12 +55,12 @@ export default async function Home() {
             />
           )
         })}
-      </div> */}
+      </div>
       <div className='homepage__contacts'>
         <p>{contact.fields.name}</p>
         <p>{contact.fields.email}</p>
         <a href={contact.fields.lindedin} target='_blank'>LinkedIn</a>
       </div>
     </div>
-  )
-}
+  );
+};
